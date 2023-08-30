40866034db269b30e714efb037f0d055e8a84a4c
unknown <skinotic@hotmail.fr>
2023-08-30 15:46:39 +0200

    version finale initialisation context

--- src/utils/context/index.jsx
+++ src/utils/context/index.jsx
@@ -1,10 +1,10 @@
-import React, { createContext } from 'react'
-import  { useMediaQuery }  from  'react-responsive'
+import React, { createContext } from 'react';
+import  { useMediaQuery }  from  'react-responsive';
 
 // Initialisation du contexte pour le thème
 export const ThemeContext = createContext()
 
-var mobile = {  mobile : 'true' };
+var mobile = {  mobile : true };
 export const ThemeProvider = ({ children }) => {
   mobile = useMediaQuery ( {  query : '(max-width : 768px)'  } ) 
   return (
